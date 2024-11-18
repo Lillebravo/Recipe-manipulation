55f102876b53eac36637d9baf85905c77d176245
Jerry <you@example.com>
2024-11-18 14:38:41 +0100

    now taking text directly from the li element in .8

--- index.js
+++ index.js
@@ -67,18 +67,12 @@ const fourthIngredient = ingredientsListPaste.children[3];
 answer(question7, fourthIngredient.textContent); 
 
 // 8. 
-const instructions = [
-  "Smula sönder 10 stycken digistivetex och blanda sen med smöret. Bred ut det i botten av en en rund form med lösbara kanter. Det ska bli som en tjockt täcke på botten av formen.",
-  "Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.",
-  "Vispa äggvitorna hårt med en elvisp. De är tillräckligt vispade när du kan vända uppochner på bunken utan att det rör sig.",
-  "Häll upp grädden i en mellanstor bunke. Vispa de relativ hårt. En aning hårdare en vanligt.",
-  "Blanda i philadelphiaosten och vaniljsockret med äggulorna och vispa ihop.",
-  "Häll nu över innehållet i bunken med äggulor över grädden. Vispa ihop på lägsta hastigheten.",
-  "Häll sedan över det innehållet till bunken med äggvitorna. Blande ihop med en gaffel tills det blir en slät fluffig smet.",
-  "Häll smeten i formen, smula över de resterande digistivetexen om du vill.",
-  "Ställ in i frysen över natten.",
-  "Servera och toppa med det du känner för."
-];
+const instructions = [];
+for (let i = 0; i < 10; i++) {
+  let instruction = `${i + 1}. ` + document.querySelectorAll(".instructions-list li")[i].textContent;
+  instruction = instruction.replace(/\s+/g, " ").trim();
+  instructions.push(instruction);
+}
 
 const arrayOfObjects = Array.from(instructions, (instruction, index) => ({
   order: index + 1,
